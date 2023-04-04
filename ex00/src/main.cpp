b3d7bb638666034f90184298b54561de7c3e6e41
Shun Usami <usatie@gmail.com>
2023-04-04 16:42:46 +0900

    :sparkles: Add WrongAnimal, WrongCat, WrongDog

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,6 +4,9 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "WrongDog.hpp"
 
 #define RESET "\033[m"
 #define GREEN "\033[0;32m"
@@ -94,9 +97,25 @@ void test_from_subject() {
   delete meta;
 }
 
+void test_wrong_animal() {
+  printTitle("Test WrongAnimal");
+  const WrongAnimal* meta = new WrongAnimal();
+  const WrongAnimal* j = new WrongDog();
+  const WrongAnimal* i = new WrongCat();
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
+  i->makeSound();  // will output the wrong animal sound!
+  j->makeSound();
+  meta->makeSound();
+  delete j;  // should not create a leak
+  delete i;
+  delete meta;
+}
+
 int main(void) {
   test_animal();
   test_dog();
   test_cat();
   test_from_subject();
+  test_wrong_animal();
 }
